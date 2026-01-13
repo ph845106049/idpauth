9ff9a439f2cf0fa9498c0b965e48a23d7eed822f
lihaiqi <lihaiqi@keyto168.com>
2026-01-13 16:16:54 +0800

    登录鉴权注册 登出

---
+++ src/main/java/com/platform/idpauth/domain/enums/AuthErrorCode.java
@@ -0,0 +1,14 @@
+package com.platform.idpauth.domain.enums;
+
+/**
+ * @author lhq
+ * @version 1.0
+ * @description:
+ * @date @date 2026年01月13日 15:57
+ */
+public enum AuthErrorCode {
+    TOKEN_MISSING,
+    TOKEN_INVALID,
+    TOKEN_EXPIRED,
+    TOKEN_REVOKED
+}
---
+++ src/main/java/com/platform/idpauth/domain/exception/AuthException.java
@@ -0,0 +1,22 @@
+package com.platform.idpauth.domain.exception;
+
+import com.platform.idpauth.domain.enums.AuthErrorCode;
+
+/**
+ * @author lhq
+ * @version 1.0
+ * @description:
+ * @date @date 2026年01月13日 15:58
+ */
+public class AuthException extends RuntimeException {
+    private final AuthErrorCode code;
+
+    public AuthException(AuthErrorCode code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public AuthErrorCode getCode() {
+        return code;
+    }
+}
